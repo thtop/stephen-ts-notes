594eee6f107fe2892f9d6932b4c5c5cd9b82aaef
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-11-06 02:36:55 +0700

    finished section 3

--- 03-Annotations/notes.md
+++ 03-Annotations/notes.md
@@ -1,17 +1,26 @@
 # Section 3: Type Annotations in Action
 
 ## Type annotations
+
 - Code we add to tell Typescript what type of value a variable will refer to
 - We (developers) tell Typescript the type
 
 ## Type inference
+
 - Typescript tries to figure out what type of value a variable refers to
 - Typescript guesses the type
 
 ## When to use
+
 - Type annotations
   - When we declare a variable on one line then initialize it later
   - When we want a variable to have a type that can't be inferred
   - When a function returns the `any` type and we need to clarify the value
 - Type inference
   - Always!
+
+## The 'Any' Type
+
+- A type, just as 'string' or 'boolean' are
+- Means TS has no idea what this is - can't check for correct property references
+- Avoid variables with 'any' at all costs
--- 03-Annotations/variables.ts
+++ 03-Annotations/variables.ts
@@ -27,3 +27,30 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20}
+
+// 2) When we ceclare a variable on one line
+// and initalizate it later
+let words = ['red', 'green', 'bule'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
