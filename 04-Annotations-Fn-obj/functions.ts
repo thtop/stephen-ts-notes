5d11155d6f103593dbf739e0c0b7abb6ffa091d1
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-11-06 09:06:52 +0700

    finished section 4

---
+++ 04-Annotations-Fn-obj/functions.ts
@@ -0,0 +1,60 @@
+// Function
+// const logNumber: (i: number) => void = (i: number) => {
+//   console.log(i);
+// };
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function(a: number, b: number): number {
+  return a * b;
+};
+
+// void
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+// never
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const throwError2 = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+  return message;
+};
+
+const throwError3 = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
+
+// Destructuring with antotations
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+const logWeather = ({
+  date,
+  weather
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
---
+++ 04-Annotations-Fn-obj/notes.md
@@ -0,0 +1,7 @@
+# Section 4: Anotations With Functions and Objects
+
+## Funcitons
+- Type annotations for function
+  - Code we add to tell Typescript what type of arguments a function will receive and what type of values it will return
+- Type inference for function
+  - Typescript tries to figure out what type of value a function will return
