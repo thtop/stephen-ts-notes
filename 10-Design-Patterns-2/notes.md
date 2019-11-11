7991da3fb9b2ee5bcdd152b7a02509aba83a10ce
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-11-11 08:56:04 +0700

    finished section 10

--- 10-Design-Patterns-2/notes.md
+++ 10-Design-Patterns-2/notes.md
@@ -39,3 +39,13 @@
   - symbol
 - Narrow down every other type of value
   - Every other value that is created with a constructor function
+
+## Interface  vs Abstract Classes
+- Interface
+  - Sets up a contract between different classes
+  - Use when we have very different objects that we want to work together
+  - Promotes loose coupling
+- Inheritance/Abstract Classes
+  - Sets up a contract between different classes
+  - Use when we are trying to build up a definition of an object
+  - Strongly couples classes together
--- 10-Design-Patterns-2/sort/build/Sorter.js
+++ 10-Design-Patterns-2/sort/build/Sorter.js
@@ -1,15 +1,19 @@
 "use strict";
+// interface Sortable {
+//   length: number;
+//   compare(leftIndex: number, rightIndex: number): boolean;
+//   swap(leftIndex: number, rightIndex: number): void;
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter = /** @class */ (function () {
-    function Sorter(conllection) {
-        this.conllection = conllection;
+    function Sorter() {
     }
     Sorter.prototype.sort = function () {
-        var length = this.conllection.length;
+        var length = this.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.conllection.compare(j, j + 1)) {
-                    this.conllection.swap(j, j + 1);
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
                 }
             }
         }
