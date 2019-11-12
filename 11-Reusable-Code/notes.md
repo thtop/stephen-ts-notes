3390dcdbe2bcae44da23275dcaf6549f0ad7b05e
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-11-13 05:09:32 +0700

    finished item 110

--- 11-Reusable-Code/notes.md
+++ 11-Reusable-Code/notes.md
@@ -2,3 +2,18 @@
 
 ## Type Definition Files
 - `npm install @types/node`
+
+
+## When to Use Enums
+- Follow near-identical syntax rules as normal object
+- Creates an object with the same keys and values whep converted from TS to JS
+- Primary goal is to signal to other engineers that these are all closely realated values 
+- Use whenever we have a small tixed set of values that are all closely related and known at compile time
+
+## Should we use an enum to represent...
+- ...primary colors on a color picker? ✔️
+- ...the set of movei categories on Netflix? ❌
+- ...the titles of all blog posts by a particular user? ❌
+- ...sizes of a drink on an ordering menu? ✔️
+- ...all years since the year 1750? ❌
+- ...the 'read' status of a text message? ✔️
--- 11-Reusable-Code/stats/src/index.ts
+++ 11-Reusable-Code/stats/src/index.ts
@@ -1,24 +1,14 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
-// enum - enumeration
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
+const dateOfFirstMatch = reader.data[0][0];
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
