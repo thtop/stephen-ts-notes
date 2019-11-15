6f33f5c66b70a94cc6f16d540aed5b5277e3fe11
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-11-16 05:24:04 +0700

    finished item 262

---
+++ 17-React-Redux-TS/rrts/src/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+interface AppProps {
+  color: string;
+}
+
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App color="red" />, document.querySelector("#root"));
