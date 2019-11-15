b5d7d79f07fa1deddef72d26bcdbb0aed1d3fb41
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-11-15 21:47:34 +0700

    finished item 240

---
+++ 16-Advanced-Express-TS/metadata/metadata.ts
@@ -0,0 +1,48 @@
+import 'reflect-metadata';
+
+@controller
+class Plane {
+  color: string = 'red';
+
+  @get('/login')
+  fly(): void {
+    console.log('vrrrrrrrr');
+  }
+}
+
+function get(path: string) {
+  return function(target: Plane, key: string) {
+    Reflect.defineMetadata('path', path, target, key);
+  };
+}
+
+function controller(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const path = Reflect.getMetadata('path', target.prototype, key);
+    const middleware = Reflect.getMetadata('middleware', target.prototype, key);
+
+    router.get(path, middleware, target.prototype[key]);
+  }
+}
+
+// const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+// console.log(secret);
+
+// const plane = {
+//   color: 'red'
+// };
+
+// Reflect.defineMetadata('note', 'hi there', plane, 'color');
+
+// const note = Reflect.getMetadata('note', plane, 'color');
+
+// console.log(note);
+
+// Reflect.defineMetadata('note', 'hi there', plane);
+// Reflect.defineMetadata('height', 10, plane);
+
+// const note = Reflect.getMetadata('note', plane);
+// const height = Reflect.getMetadata('height', plane);
+
+// console.log(note);
+// console.log(height);
---
+++ 16-Advanced-Express-TS/notes.md
@@ -0,0 +1,8 @@
+# Advanced Express and TS Intergration
+
+## Matadata
+- Proposed feature to be added to JavaScript (and thus, TS)
+- Snippets of info that can be tied to a method, property, or class definition
+- Can be used for super custom stuff
+- Typescript will (optionally) provide type information as metadata
+- Read and written using the reflect-metadata package
