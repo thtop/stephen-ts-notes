aa52874e80190fe4c4215567d0c6f9a26b7467be
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-11-15 11:27:49 +0700

    finished section 13

--- 13-Web-Framework/web/src/index.ts
+++ 13-Web-Framework/web/src/index.ts
@@ -1,8 +1,39 @@
+// input and buttons
+/*
 import { UserForm } from './views/UserForm';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'NAME', age: 20 });
 
-const userForm = new UserForm(document.getElementById('root'), user);
+const root = document.getElementById('root');
 
-userForm.render();
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
+
+*/
+
+// User List
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
+
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
